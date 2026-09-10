e19eae1b4b1065a608a5f6dd294583076715d321
berksvml <berksvmli@gmail.com>
2026-09-10 11:23:16 +0200

    chore: initialize Unreal Engine project

---
+++ Source/C152FlightSim/C152FlightSim.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "C152FlightSim.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, C152FlightSim, "C152FlightSim" );
